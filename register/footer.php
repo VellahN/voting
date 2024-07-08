4b0dea1f48f3fd5c02f76b58c34c09d5d0509835
VellahN <92290865+VellahN@users.noreply.github.com>
2024-07-08 17:49:48 +0300

    online voting system

---
+++ register/footer.php
@@ -0,0 +1,12 @@
+
+<div class="footer">
+	<hr/>
+&copy2024
+<br/>
+Online Voting System.
+<br/>
+Zetech University
+<br/>
+Contact: <a href="tel:+254 719 034 500">+254 719 034 500</a> | Email: <a href="mailto:info@zetech.ac.ke">info@zetech.ac.ke</a>
+
+</div>
